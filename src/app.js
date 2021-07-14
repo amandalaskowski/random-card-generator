39ec4476c9c7a21c8e063ac50126c647aa26edf0
amandalaskowski <amanda.2002@aol.com>
2021-07-14 00:11:39 +0000

    hi leo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
-/* eslint-disable */
-
+import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let cardNumbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "King",
+    "Queen",
+    "Ace"
+  ];
+  let cardSuits = ["heart", "spades", "clubs", "diamonds"];
+  let randomCardNumber =
+    cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+  let randomCardSuits = cardSuits[Math.floor(Math.random() * cardSuits.length)];
+  let cardSuit = document.querySelector(".card h1");
+  cardSuit.innerHTML = randomCardNumber;
+  card.classList.add(randomCardSuits);
 };
